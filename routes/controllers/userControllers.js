9ed95e23a3f28721e2852ee5174e9975614cffeb
syedarhamraza <arhamraza399@gmail.com>
2026-06-23 11:31:43 +0500

    added express backend

--- routes/controllers/userControllers.js
+++ routes/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const app = require("../../app");
 const fs = require("fs");
 
-exports.getAlluser = async (req, res) =>{
+exports.getAlluser = async (req, res) => {
     res.status(200).json({
         status: 'success',
         data: {
@@ -10,15 +10,15 @@ exports.getAlluser = async (req, res) =>{
     })
 }
 
-exports.createUser = async (req, res) =>{
-    const data = await  req.body;
+exports.createUser = async (req, res) => {
+    const data = await req.body;
     res.status(201).json({
         status: 'success',
         data,
     })
 }
 
-exports.getUser = async (req, res) =>{
+exports.getUser = async (req, res) => {
     const id = parseInt(req.params.id)
     res.status(200).json({
         status: 'success',
